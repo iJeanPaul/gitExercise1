5e2a078493be3065fce0bead61313ed9240f4c77
DESKTOP-PKDN22A\Jean Paul Iradukunda <jeanpaul.iradukunda@gmail.com>
2016-10-31 22:23:30 -0500

    Added a few WriteLines in MyClass

--- gitExercise1/MyClass.cs
+++ gitExercise1/MyClass.cs
@@ -11,6 +11,9 @@ namespace gitExercise1
         public static void hello()
         {
             Console.WriteLine("method 'hello' says: hello");
+            Console.WriteLine("Added Line 01 in MyClass for commit 02");
+            Console.WriteLine("Added Line 02 in MyClass for commit 02");
+            Console.WriteLine("Added Line 03 in MyClass for commit 02");
         }
     }
 }
