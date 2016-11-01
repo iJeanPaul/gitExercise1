b9f5ebc2841ba7720bc1485ad988e756d5e8f6c9
DESKTOP-PKDN22A\Jean Paul Iradukunda <jeanpaul.iradukunda@gmail.com>
2016-10-31 22:30:50 -0500

    Additional WriteLines in Main

--- gitExercise1/Program.cs
+++ gitExercise1/Program.cs
@@ -15,6 +15,10 @@ namespace gitExercise1
             Console.WriteLine("Added Line 01 for testing commit");
             Console.WriteLine("Added Line 02 for testing commit");
             Console.WriteLine("Added Line 03 for testing commit");
+
+            Console.WriteLine("Added Line 04 for another commit");
+            Console.WriteLine("Added Line 05 for another commit");
+            Console.WriteLine("Added Line 06 for another commit");
         }
     }
 }
